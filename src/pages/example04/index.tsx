eea8dadd39a7b34eefa299730a03bb542b13ef80
André Souza <andresouza@meraki.dev.br>
2023-09-16 02:11:38 -0300

    UPDATE

--- src/pages/example04/index.tsx
+++ src/pages/example04/index.tsx
@@ -1,21 +1,22 @@
 import { FC } from "react";
-import { Container } from "../../components/Container";
 import { Heading2, Paragraph } from "../../components/Heading";
 import { Button } from "../../components/Button";
 
 const Example04: FC = () => {
   return (
-    <Container bg={"bg-[#252B42]"}>
-      <section className="max-w-[1050px] mx-auto h-[100vh] flex items-center justify-center flex-col gap-[30px] z-[15] relative">
-        <Heading2 color="text-white">Designing Better Experience</Heading2>
-        <Paragraph color="text-white">
-          Problems trying to resolve the conflict between the two major realms
-          of Classical physics: Newtonian mechanics
-        </Paragraph>
-        <Button>ADD YOUR CALL TO ACTION</Button>
-        <Paragraph weight="bold" color="text-white"> Designing Better Experience</Paragraph>
+    <main className="w-[100vw] flex items-center justify-center">
+      <section className="max-w-[1050px] mx-auto flex items-center p-[30px] justify-center flex-col">
+        <div className="flex items-center justify-center flex-col gap-[30px]">
+          <Heading2>Problems trying to resolve the conflict between</Heading2>
+          <Paragraph>
+            Problems trying to resolve the conflict between the two major realms
+            of Classical physics:
+          </Paragraph>
+          <Button>ADD YOUR CALL TO ACTION</Button>
+        </div>
+        <img src="./hyperspace_maintenance_day.png" alt="" />
       </section>
-    </Container>
+    </main>
   );
 };
 export { Example04 };
